d09f192210c16f029f1a33f5c87091a64fa27951
Edson Copque <ecop@disroot.org>
2024-12-08 11:12:47 -0300

    Create 24_-_exercicio_soma_lista.py

---
+++ 002/24_-_exercicio_soma_lista.py
@@ -0,0 +1,15 @@
+# x = [float(input()) for i in range(0, 10)]
+# print(x)
+# print(sum(x))
+
+hd_number = []
+for i in range(0, 10):
+    hd_number.append(float(input('Digite os números: ')))
+print(hd_number)
+
+hd_sum = 0
+for i in hd_number:
+    hd_sum += i
+print(hd_sum)
+
+# Edson Copque | https://linktr.ee/edsoncopque
