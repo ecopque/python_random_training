0968cbfe9ba65d60da1f3b30dfe69c3381253573
Edson Copque <ecop@disroot.org>
2024-12-07 23:07:07 -0300

    Update 19-3_-_lacos_for_complexidade_algoritmica.py

--- 002/19-3_-_lacos_for_complexidade_algoritmica.py
+++ 002/19-3_-_lacos_for_complexidade_algoritmica.py
@@ -2,4 +2,14 @@ for i in range(0, 3):
     for j in range(0, 3):
         print(f'i={i} j={j}')
 
-# Edson Copque | 
+print()
+
+hd = 0
+for i in range(0, 100):
+    for j in range(0, 100):
+        for k in range(0, 100):
+            print(f'i={i} j={j} l={k}')
+            hd +=1
+print(hd)
+
+# Edson Copque | https://linktr.ee/edsoncopque
