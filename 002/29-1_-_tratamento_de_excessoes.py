e26c4e59c72e86c3bf8f2c4f56fd58031b84a501
Edson Copque <ecop@disroot.org>
2024-12-09 15:30:21 -0300

    Create 29-1_-_tratamento_de_excessoes.py

---
+++ 002/29-1_-_tratamento_de_excessoes.py
@@ -0,0 +1,11 @@
+n1 = int(input('Enter a number: '))
+n2 = int(input('Enter another number: '))
+
+try:
+    print(n1 / n2)
+except:
+    print('Msg except.')
+finally:
+    print('Msg finally.')
+
+# Edson Copque | https://linktr.ee/edsoncopque
