e2dfb0bdd27668ba0d3d2dd17c3bce1caf3abc08
Edson Copque <ecop@disroot.org>
2024-12-10 20:21:48 -0300

    Create 3_-_metodo_construtor.py

---
+++ 003/3_-_metodo_construtor.py
@@ -0,0 +1,21 @@
+class Person:
+    def __init__(self): # constructor method (__) | automatic
+        print(f'Hello')
+
+    def method_message(self):
+        print('Method message.')
+
+p1 = Person()
+# Response: Hello
+
+p1.method_message()
+# Method message
+
+class NewPerson():
+    def __init__(self, name, age, height):
+        print(f'{name} | {age} | {height}')
+
+p2 = NewPerson('Edson', 50, 70)
+# Response: Edson | 50 | 70
+
+# Edson Copque | https://linktr.ee/edsoncopque
