499c6be39ab6a9426ec5a8a11ff2d4806cfa9fa4
Edson Copque <ecop@disroot.org>
2024-12-10 19:51:22 -0300

    Create 1_-_introducao_a_poo.py

---
+++ 003/1_-_introducao_a_poo.py
@@ -0,0 +1,12 @@
+# procedural, orientado à objetos, funcional
+
+x = []
+for i in range(0, 10):
+    if i % 2==0:
+        x.append(i)
+print(x)
+
+y = [i for i in range(0, 10) if i % 2==0]
+print(y)
+
+# Edson Copque | https://linktr.ee/edsoncopque
