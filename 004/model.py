a65d142d875c00264c87c0464c2e3a622c77e611
Edson Copque <ecop@disroot.org>
2024-12-12 17:12:02 -0300

    Create model.py

---
+++ 004/model.py
@@ -0,0 +1,9 @@
+# Data modeling
+
+class PersonModel:
+    def __init__(self, name, age, cpf):
+        self.name = name
+        self.age = age
+        self.cpf = cpf
+
+# Edson Copque | https://linktr.ee/edsoncopque
