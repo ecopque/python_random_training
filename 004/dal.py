cd2298249ea05b87121b9b8b60bc11e757ffe2da
Edson Copque <ecop@disroot.org>
2024-12-12 17:28:31 -0300

    Update dal.py

--- 004/dal.py
+++ 004/dal.py
@@ -9,19 +9,24 @@ class PersonDal:
             file.write(person.name + ' ' + str(person.age) + ' ' + str(person.cpf))
     
     @classmethod
-    def readtal(cls):
-        name = 'Edson'
-        age = 100
-        cpf = 123456
+    def readdal(cls):
+        # name = 'Edson'
+        # age = 100
+        # cpf = 123456
+        with open('persons.txt', 'r') as file:
+            data = file.read().split()
+            name = data[0]
+            age = int(data[1])
+            cpf = int(data[2])
         return PersonModel(name, age, cpf)
 
-# Creating a Personodel instance:
+# Creating a PersonModel instance:
 p1 = PersonModel('Edsuu', 50, 987654321)
 
 # Saving data to file:
 PersonDal.savedal(p1)
 
 # Reading data:
-print(PersonDal.readtal().cpf)
+print(PersonDal.readdal().cpf)
 
 # Edson Copque | https://linktr.ee/edsoncopque
