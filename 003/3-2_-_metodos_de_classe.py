54207d631c74181e6c7bb843cac99292b3a20a5a
Edson Copque <ecop@disroot.org>
2024-12-12 10:38:58 -0300

    Update 3-2_-_metodos_de_classe.py

--- 003/3-2_-_metodos_de_classe.py
+++ 003/3-2_-_metodos_de_classe.py
@@ -12,20 +12,25 @@ class Person:
         print(self.my_friend())
 
     def my_new_method(self):
-        return '888'
+        xxx = '888'
+        return xxx
 
     def change_x(self, new_value):
         Person.x = new_value
 
     @classmethod
     def my_class_method(cls, parameter):
-        print(f'New method: {parameter}')
+        cls.y = 999 # see class attribute 'y'
+        print(f'New method: {parameter} and {cls.y}')
 
 p1 = Person('Edson')
 p1.my_class_method(55)
 # Response: New method: 55
 
-Person.my_class_method(11)
-# Response: New method: 11
+p2 = Person('John')
+print(p2.y) # response: 999
+
+Person.my_class_method(11) # wiwhout 'p1'
+# Response: New method: 11 and 999
 
 # Edson Copque | https://linktr.ee/edsoncopque
